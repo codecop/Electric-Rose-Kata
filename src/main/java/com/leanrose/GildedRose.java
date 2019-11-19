e70adb6052d4e54247a58ce068ba71654b73e41f
Peter Kofler <peter.kofler@code-cop.org>
2019-11-19 21:16:51 +0100

    Finish removing sulfuras.

--- src/main/java/com/leanrose/GildedRose.java
+++ src/main/java/com/leanrose/GildedRose.java
@@ -12,9 +12,7 @@ class GildedRose {
             if (!items[i].name.equals("Aged Brie")
                     ) {
                 if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
+                    items[i].quality = items[i].quality - 1;
                 }
             } else {
                 if (items[i].quality < 50) {
@@ -22,16 +20,12 @@ class GildedRose {
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+            items[i].sellIn = items[i].sellIn - 1;
 
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (items[i].quality > 0) {
-                        if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                            items[i].quality = items[i].quality - 1;
-                        }
+                        items[i].quality = items[i].quality - 1;
                     }
                 } else {
                     if (items[i].quality < 50) {
--- src/main/java/com/leanrose/TexttestFixture.java
+++ src/main/java/com/leanrose/TexttestFixture.java
@@ -7,11 +7,7 @@ public class TexttestFixture {
         Item[] items = new Item[] { //
                 new Item("+5 Dexterity Vest", 10, 20), //
                 new Item("Aged Brie", 2, 0), //
-                new Item("Elixir of the Mongoose", 5, 7), //
-                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-                // this conjured item does not work properly yet
-                new Item("Conjured Mana Cake", 3, 6) };
+                new Item("Elixir of the Mongoose", 5, 7)}; //
 
         GildedRose app = new GildedRose(items);
 
